adfa686bd5b7eda30a4e628679ec367f1c226b33
CalinMG <calin18mak@gmail.com>
2026-02-25 10:19:05 +0200

    Lab 1: HTML and CSS review

---
+++ laborator-1/js/contact_form.js
@@ -0,0 +1,10 @@
+function submitForm() { 
+	const nume=document.getElementById("name").value;
+	const email=document.getElementById("email").value;
+	const text=document.getElementById("text").value;
+	console.log(name);
+	console.log(email);
+	console.log(text);
+	console.warn("Goodbye World")
+} 
+
