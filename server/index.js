091f7a5455943ad88999695be425812fb128fd9a
CalinMG <calin18mak@gmail.com>
2026-05-21 22:33:43 +0300

    10.1

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express'); 
 const app = express(); 
+const cors = require('cors'); 
+app.use(cors()); 
 const Project = require('./models/Project'); 
 const PORT = 3000; 
 const mongoose = require('mongoose');
