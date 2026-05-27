839c7180db1d66d7b7c756a54d8abee4352633a4
CalinMG <calin18mak@gmail.com>
2026-05-27 12:45:25 +0300

    CSS

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -11,6 +11,7 @@ import TodoList from './TodoList';
 import ContactForm from './ContactForm';
 import ProjectList from './ProjectList';
 import Navbar from './Navbar';
+import './App.css'
 function App() {
 
     return ( 
